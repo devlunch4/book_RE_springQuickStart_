1b97c187c888a0a4d3d0b69d33984639d1ac07ee
devlunch4@E470 <devlunch4@gmail.com>
2022-01-28 00:55:12 +0900

    4.3.1 end/

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -8,14 +8,13 @@ public class SamsungTV implements TV {
 		System.out.println("===> SamsungTV(1) 按眉 积己");
 	}
 
-	public SamsungTV(Speaker speaker) {
-		System.out.println("===> SamsungTV(2) 按眉 积己");
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker() 龋免");
 		this.speaker = speaker;
 	}
 
-	public SamsungTV(Speaker speaker, int price) {
-		System.out.println("===> SamsungTV(3) 按眉 积己");
-		this.speaker = speaker;
+	public void setPrice(int price) {
+		System.out.println("===> setPrice() 龋免");
 		this.price = price;
 	}
 
