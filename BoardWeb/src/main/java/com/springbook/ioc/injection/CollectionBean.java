94b9509fdd5b95732d2629f8a86070cac636a609
devlunch4@E470 <devlunch4@gmail.com>
2022-01-30 17:30:31 +0900

    4.4.2

--- BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBean.java
+++ BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBean.java
@@ -1,15 +1,15 @@
 package com.springbook.ioc.injection;
 
-import java.util.List;
+import java.util.Set;
 
 public class CollectionBean {
-	private List<String> addressList;
+	private Set<String> addressList;
 
-	public void setAddressList(List<String> addressList) {
+	public void setAddressList(Set<String> addressList) {
 		this.addressList = addressList;
 	}
 
-	public List<String> getAddressList() {
+	public Set<String> getAddressList() {
 		return addressList;
 	}
 }
--- BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
+++ BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
@@ -1,6 +1,6 @@
 package com.springbook.ioc.injection;
 
-import java.util.List;
+import java.util.Set;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -9,7 +9,8 @@ public class CollectionBeanClient {
 	public static void main(String[] args) {
 		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
 		CollectionBean bean = (CollectionBean) factory.getBean("collectionBean");
-		List<String> addressList = bean.getAddressList();
+		
+		Set<String> addressList = bean.getAddressList();
 		for (String address : addressList) {
 			System.out.println(address.toString());
 		}
